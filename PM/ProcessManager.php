424f3f8f244d548237594b35e0ad545581da4a6a
Serg Nochevny <serg.nochevny@engineering.ait.com>
2018-04-17 21:46:24 +0300

    ....................

--- PM/ProcessManager.php
+++ PM/ProcessManager.php
@@ -482,18 +482,19 @@ class ProcessManager{
                 $this->output->write("<error>$data</error>");
             }
         );
-        $process->stdout->on(
-            'data',
-            function($data) use ($port){
-                if($this->output->isVeryVerbose()) {
-                    if($this->lastWorkerErrorPrintBy !== $port) {
-                        $this->output->writeln("<info>--- Worker $port stdout ---</info>");
-                        $this->lastWorkerErrorPrintBy = $port;
-                    }
-                    $this->output->write("<info>$data</info>");
-                }
-            }
-        );
+
+//        $process->stdout->on(
+//            'data',
+//            function($data) use ($port){
+//                if($this->output->isVeryVerbose()) {
+//                    if($this->lastWorkerErrorPrintBy !== $port) {
+//                        $this->output->writeln("<info>--- Worker $port stdout ---</info>");
+//                        $this->lastWorkerErrorPrintBy = $port;
+//                    }
+//                    $this->output->write("<info>$data</info>");
+//                }
+//            }
+//        );
 
         if($this->output->isVeryVerbose()) {
             $this->output->writeln(sprintf("Worker pid %d has been started", $process->getPid()));
