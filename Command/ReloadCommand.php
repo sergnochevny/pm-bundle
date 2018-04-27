0fb28690b2b66e6d2de96131e906982fa857dc27
Serg Nochevny <serg.nochevny@engineering.ait.com>
2018-04-27 12:44:18 +0300

    .

--- Command/ReloadCommand.php
+++ Command/ReloadCommand.php
@@ -1,42 +1,46 @@
 <?php
 
-namespace PHPPM\Commands;
+namespace Other\PmBundle\Command;
 
-use PHPPM\ProcessClient;
+use Other\PmBundle\PM\ProcessClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ReloadCommand extends Command
-{
+class ReloadCommand extends Command{
+
     use ConfigTrait;
 
     /**
      * {@inheritdoc}
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
      */
-    protected function configure()
-    {
+    protected function configure(){
         parent::configure();
 
         $this
-            ->setName('reload')
+            ->setName('pmb:reload')
             ->setDescription('Reloads the server')
             ->addOption('socket-path', null, InputOption::VALUE_REQUIRED, 'Path to a folder where socket files will be placed. Relative to working-directory or cwd()', '.ppm/run/')
-            ->addArgument('working-directory', InputArgument::OPTIONAL, 'Working directory', './')
-        ;
+            ->addArgument('working-directory', InputArgument::OPTIONAL, 'Working directory', './');
 
         $this->configurePPMOptions($this);
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|null|void
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output){
         $config = $this->initializeConfig($input, $output, false);
 
         $handler = new ProcessClient();
         $handler->setSocketPath($config['socket-path']);
-        $handler->reloadProcessManager(function ($status) use ($output) {
+        $handler->reloadProcessManager(function($status) use ($output){
             $output->writeln("Requested a worker reload.");
         });
 
